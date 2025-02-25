e421cb845493467930019aca5640935fab0a776a
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-02-25 15:49:22 +0000

    add error in router.ts and add errorHandlerMiddleware in server.ts

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import { HttpError } from "./errors/HttpError";
 
 const router = Router()
 
-router.get("/status", (req,res) => {
-  res.json( {message: "Test route"} )
+router.get("/status", async (req,res, next) => {
+  try {
+    throw new HttpError(401, "Unauthorized")
+    res.json({ message: "OK" })
+  } catch (error) {
+    next(error)
+  }
 })
 
 export { router }
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import cors from "cors"
 import express from "express"
 import { router } from "./router"
+import { errorHandlerMiddleware } from "./middlewares/error-handler"
 
 const app = express()
 
 app.use(cors())
 app.use("/api", router)
+app.use(errorHandlerMiddleware)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server ir running on http://localhost:${PORT}`))
