4263646bbc6bb8ee2198628749a13c7f8cafdb77
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-02-27 12:28:00 +0000

    feat(routes): add PUT /leads/:id route in router.ts

--- src/router.ts
+++ src/router.ts
@@ -9,6 +9,7 @@ const leadsController = new LeadsController()
 router.get("/leads", leadsController.index)
 router.post("/leads", leadsController.create)
 router.get("/leads/:id", leadsController.show)
+router.put("/leads/:id", leadsController.update)
 
 router.get("/status", async (req,res, next) => {
   try {
