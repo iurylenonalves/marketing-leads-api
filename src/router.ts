ddda1e73d1925103cca012e985d0a7791425c686
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-02-27 13:33:26 +0000

    feat(routes): add DELETE /leads/:id route in router.ts

--- src/router.ts
+++ src/router.ts
@@ -10,6 +10,7 @@ router.get("/leads", leadsController.index)
 router.post("/leads", leadsController.create)
 router.get("/leads/:id", leadsController.show)
 router.put("/leads/:id", leadsController.update)
+router.delete("/leads/:id", leadsController.delete)
 
 router.get("/status", async (req,res, next) => {
   try {
