591f7408ded06d44a87bb04eda7df8eedc4c7fb8
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-02-27 01:31:45 +0000

    feat(routes): add POST /leads route in router.ts

--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,7 @@ const router = Router()
 const leadsController = new LeadsController()
 
 router.get("/leads", leadsController.index)
+router.post("/leads", leadsController.create)
 
 router.get("/status", async (req,res, next) => {
   try {
