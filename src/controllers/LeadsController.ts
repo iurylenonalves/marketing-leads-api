7144f1aac9089d1b3826248137de691bf3b16b19
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-02-27 01:28:44 +0000

    feat(controllers): add create method in LeadsController.ts

--- src/controllers/LeadsController.ts
+++ src/controllers/LeadsController.ts
@@ -1,11 +1,24 @@
 import { Handler } from "express";
 import { prisma } from "../database";
+import { CreateLeadRequestSchema } from "../schemas/LeadsRequestSchema";
 
 export class LeadsController {
   index: Handler = async (req, res, next) => {
     try {
       const leads = await prisma.lead.findMany()
       res.json(leads)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  create: Handler = async (req, res, next) => {
+    try {
+      const body = CreateLeadRequestSchema.parse(req.body)
+      const newLead = await prisma.lead.create({
+        data: body
+      })
+      res.status(201).json(newLead)
     } catch (error) {
       next(error)
     }
