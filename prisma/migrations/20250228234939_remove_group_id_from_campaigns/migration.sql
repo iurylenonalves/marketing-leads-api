dbf4b6a570e2ea48acf5393c2ad24204c3300323
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-02-28 23:59:36 +0000

    refactor(schema): remove groupId from Campaign model

---
+++ prisma/migrations/20250228234939_remove_group_id_from_campaigns/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `groupId` on the `Campaign` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Campaign" DROP COLUMN "groupId";
