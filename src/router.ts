0861f56275980222ea8334493bde4e51507072ca
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-02-28 00:39:59 +0000

    feat(router): add route for listing groups

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,21 @@
 import { Router } from "express";
 import { HttpError } from "./errors/HttpError";
 import { LeadsController } from "./controllers/LeadsController";
+import { GroupsController } from "./controllers/GroupsController";
 
 const router = Router()
 
 const leadsController = new LeadsController()
+const groupsController = new GroupsController
 
 router.get("/leads", leadsController.index)
 router.post("/leads", leadsController.create)
 router.get("/leads/:id", leadsController.show)
 router.put("/leads/:id", leadsController.update)
 router.delete("/leads/:id", leadsController.delete)
+
+router.get("/groups", groupsController.index)
+
 
 router.get("/status", async (req,res, next) => {
   try {
