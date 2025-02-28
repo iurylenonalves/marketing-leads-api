eaabb37b657111215447e886ce635ec85a53a3fe
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-02-28 19:53:19 +0000

    feat(routes): add GET /groups/:id route in router.ts

--- src/router.ts
+++ src/router.ts
@@ -16,6 +16,7 @@ router.delete("/leads/:id", leadsController.delete)
 
 router.get("/groups", groupsController.index)
 router.post("/groups", groupsController.create)
+router.get("/groups/:id", groupsController.show)
 
 
 router.get("/status", async (req,res, next) => {
