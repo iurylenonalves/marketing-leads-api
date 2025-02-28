8d9ecc387fdc777ff2692d7830bff2e49b4bbb24
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-02-28 01:58:48 +0000

    feat(controllers): add create method in GroupsController.ts

--- src/controllers/GroupsController.ts
+++ src/controllers/GroupsController.ts
@@ -1,11 +1,24 @@
 import { Handler } from "express";
 import { prisma } from "../database";
+import { CreateGroupsRequestSchema } from "../schemas/GroupsRequestSchema";
 
 export class GroupsController {
   index: Handler = async (req, res, next) => {
     try {
       const groups = await prisma.group.findMany()
       res.json(groups)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  create: Handler = async (req, res, next) => {
+    try {
+      const body = CreateGroupsRequestSchema.parse(req.body)
+      const newGroup = await prisma.group.create({
+        data: body
+      })
+      res.status(201).json(newGroup)        
     } catch (error) {
       next(error)
     }
