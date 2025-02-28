893b86b8bfe508cdd1e1eda6a4b1fad4a66a95fd
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-02-28 19:52:33 +0000

    feat(controllers): add show method in GroupsController.ts

--- src/controllers/GroupsController.ts
+++ src/controllers/GroupsController.ts
@@ -1,6 +1,7 @@
 import { Handler } from "express";
 import { prisma } from "../database";
 import { CreateGroupsRequestSchema } from "../schemas/GroupsRequestSchema";
+import { HttpError } from "../errors/HttpError";
 
 export class GroupsController {
   index: Handler = async (req, res, next) => {
@@ -19,6 +20,21 @@ export class GroupsController {
         data: body
       })
       res.status(201).json(newGroup)        
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  show: Handler = async (req, res, next) => {
+    try {
+      const group = await prisma.group.findUnique({
+        where: { id: Number(req.params.id) },
+        include: { leads: true}
+      })
+
+      if (!group) throw new HttpError(404, "group not found");
+
+      res.json(group)
     } catch (error) {
       next(error)
     }
