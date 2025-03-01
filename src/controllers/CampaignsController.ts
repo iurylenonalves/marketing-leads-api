de2591920d75c292dfd37ed8a40a546eb83c8d31
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-03-01 00:19:13 +0000

    feat(controllers): add create method in CampaignsController.ts

--- src/controllers/CampaignsController.ts
+++ src/controllers/CampaignsController.ts
@@ -1,11 +1,23 @@
 import { Handler } from "express";
 import { prisma } from "../database";
+import { createCampaignsRequestSchema } from "../schemas/CampaignsRequestSchema";
+import { HttpError } from "../errors/HttpError";
 
 export class CampaignsController {
   index: Handler = async (req, res, next) => {
     try {
       const campaigns = await prisma.campaign.findMany()
       res.json(campaigns)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  create: Handler = async (req, res, next) => {
+    try {
+      const body = createCampaignsRequestSchema.parse(req.body)
+      const newCampaign = await prisma.campaign.create({ data: body})
+      res.status(201).json(newCampaign)
     } catch (error) {
       next(error)
     }
